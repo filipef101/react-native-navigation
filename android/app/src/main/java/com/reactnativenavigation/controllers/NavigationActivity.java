a00344e7d8922cfdf19dccf3c68a53821ae93761
Filipe Fernandes <filipe.fernandes@logirix.com>
2018-02-01 17:01:04 +0000

    Tab based navigation drawer blue shadow Disable back key closing the app

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -197,7 +197,7 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
     @Override
     public void invokeDefaultOnBackPressed() {
         if (layout != null && !layout.onBackPressed()) {
-            super.onBackPressed();
+            moveTaskToBack(true);
         }
     }
 
